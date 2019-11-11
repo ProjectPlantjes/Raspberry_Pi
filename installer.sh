b2adc8e4ce4b041e14e06877b240fdcac7a69c2d
Thijs Tops <thijs@thijstops.com>
2019-11-11 15:07:00 +0000

    Added screen to installer

--- installer.sh
+++ installer.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Install mosquitto"
-sudo apt-get install mosquitto
+sudo apt-get install mosquitto -y
 
 echo "Updating and installing bluetooth"
-sudo apt-get install bluetooth bluez blueman
+sudo apt-get install bluetooth bluez blueman -y
 
-echo "More things to be added"
+echo "Installing screen (handy tool)"
+sudo apt-get install screen -y
