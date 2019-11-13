e566c5edb5f68a5e81e4345327bc6f39fd7cb7f1
Thijs Tops <thijs@thijstops.com>
2019-11-13 10:48:06 +0000

    Thingsboard stuff added

---
+++ thingsboard/get-resources.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget http://source.thijstops.com/attributes-data.json
+wget https://raw.githubusercontent.com/thingsboard/thingsboard.github.io/master/docs/getting-started-guides/resources/mosquitto.sh
+
+echo "CHANGE THE THINGS IN mosquitto.sh!"
